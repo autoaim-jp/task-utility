c0ea675dca5d9c7267ac8cf402aa756d29400b1e
autoaim_jp <git@autoaim.jp>
2025-08-07 08:52:54 +0900

    update

--- .bashrc.d/share-my-script-to-github-for-saving-my-time.sh
+++ .bashrc.d/share-my-script-to-github-for-saving-my-time.sh
@@ -5,7 +5,13 @@ share-my-script-to-github-for-saving-my-time() {
     rm -rf /tmp/task-utility/
     git clone git@github.com:autoaim-jp/task-utility.git
     rm -rf /tmp/task-utility/.bashrc.d/
+    
+    cp -r ~/monorepo/gitlab/script/bashrc-script.sh /tmp/task-utility/
+    cp -r ~/monorepo/gitlab/script/register-all-label.sh /tmp/task-utility/
     cp -r ~/monorepo/gitlab/script/.bashrc.d/ /tmp/task-utility/
+    cp -r ~/monorepo/.gitlab/issue_templates/ /tmp/task-utility/
+    cp -r ~/monorepo/.gitlab/merge_request_templates/ /tmp/task-utility/
+
     cd /tmp/task-utility/
     git add .
     git commit -m 'update'
